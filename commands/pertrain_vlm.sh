c6819d10c459bbbd00d1092688e0538f41203fcb
HeegerGao <heegergao@gmail.com>
2025-06-04 13:24:16 +0800

    update

--- commands/pertrain_vlm.sh
+++ commands/pertrain_vlm.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=.:$PYTHONPATH
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-torchrun --nnodes=1 --nproc_per_node=8 scripts/pretrain.py
+torchrun --nnodes=1 --nproc_per_node=8 scripts/pretrain.py "$@"
